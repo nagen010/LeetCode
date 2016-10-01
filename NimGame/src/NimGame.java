9ee9d982cb81078412ad3ec90098e6dbbbedb9b1
nagen <nagendran010@gmail.com>
2016-09-30 21:05:07 -0400

    Adding test to Nim Game

--- NimGame/src/NimGame.java
+++ NimGame/src/NimGame.java
@@ -10,5 +10,7 @@ public class NimGame {
     }
     public static void main(String[] args) {
         System.out.println(canWinNim(5));
+        System.out.println(canWinNim(4));
+        System.out.println(canWinNim(12344));
     }
 }
