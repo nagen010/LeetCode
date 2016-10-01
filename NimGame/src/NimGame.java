f325dcecf79e66feb1c8bb7bd9dfb499a6fa2e6a
nagen <nagendran010@gmail.com>
2016-09-30 21:04:33 -0400

    Adding Nim Game

---
+++ NimGame/src/NimGame.java
@@ -0,0 +1,14 @@
+/** NimGame: You are playing the following Nim Game with your friend:
+ *  There is a heap of stones on the table, each time one of you take turns to remove 1 to 3 stones.
+ *  The one who removes the last stone will be the winner.
+ *  You will take the first turn to remove the stones.
+ * Created by nagen on 9/30/16.
+ */
+public class NimGame {
+    private static boolean canWinNim(int n) {
+        return n%4 > 0;
+    }
+    public static void main(String[] args) {
+        System.out.println(canWinNim(5));
+    }
+}
