9fe9bb1bbe927b20436a60ae51c20be6aba42cf8
nagen <nagendran010@gmail.com>
2016-10-02 09:28:29 -0400

    Adding RemoveElement

---
+++ RemoveElement/src/RemoveElement.java
@@ -0,0 +1,23 @@
+/** Given an array and a value, remove all instances of that value in place and return the new length.
+
+ Do not allocate extra space for another array, you must do this in place with constant memory.
+
+ The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+
+ * Created by nagen on 10/2/16.
+ */
+public class RemoveElement {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{1, 2, 3, 4, 3}, 3));
+    }
+
+    private static int removeElement(int[] array, int val) {
+        int length = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] != val)
+                array[length++] = array[i];
+        }
+        return length;
+    }
+}
