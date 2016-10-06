195c6c6b5d8c9764af000b7e2857959caebeb5af
nagen <nagendran010@gmail.com>
2016-10-06 19:56:58 -0400

    Adding SingleNumber

---
+++ SingleNumber/src/SingleNumber.java
@@ -0,0 +1,17 @@
+/** Given an array of integers, every element appears twice except for one. Find that single one.
+ * Created by nagen on 10/6/16.
+ */
+public class SingleNumber {
+    public static void main(String[] args) {
+        SingleNumber sn = new SingleNumber();
+        System.out.println(sn.singleNumber(new int[]{1,1,2,2,3}));
+    }
+
+    private int singleNumber(int[] array) {
+        int result = 0;
+        for (int anArray : array) {
+            result ^= anArray;
+        }
+        return result;
+    }
+}
