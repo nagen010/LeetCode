1c1b9913907406c651a2115bdc8544ef42bc7699
nagen <nagendran010@gmail.com>
2016-10-06 19:58:44 -0400

    Adding test to SingleNumber

--- SingleNumber/src/SingleNumber.java
+++ SingleNumber/src/SingleNumber.java
@@ -5,6 +5,7 @@ public class SingleNumber {
     public static void main(String[] args) {
         SingleNumber sn = new SingleNumber();
         System.out.println(sn.singleNumber(new int[]{1,1,2,2,3}));
+        System.out.println(sn.singleNumber(new int[]{1,1,2,2,3,3,7}));
     }
 
     private int singleNumber(int[] array) {
