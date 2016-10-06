e891d11680497069970b125fe54e44c5f1846d15
nagen <nagendran010@gmail.com>
2016-10-05 20:27:00 -0400

    Adding SumOfTwoIntegers

---
+++ SumOfTwoIntegers/src/SumOfTwoIntegers.java
@@ -0,0 +1,18 @@
+/** Calculate the sum of two integers a and b,
+ * but you are not allowed to use the operator + and -.
+ * Created by nagen on 10/5/16.
+ */
+public class SumOfTwoIntegers {
+
+    public static void main(String[] args) {
+        SumOfTwoIntegers sum = new SumOfTwoIntegers();
+        System.out.println(sum.add(1,2));
+    }
+
+    private int add(int a, int b) {
+        if (b == 0)
+            return a;
+        else
+            return add(a^b, (a&b) << 1);
+    }
+}
